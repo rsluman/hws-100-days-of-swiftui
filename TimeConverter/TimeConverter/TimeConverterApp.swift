0ee6578f16ae3d15210f8db3ea35be8b048bcf4e
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-15 19:01:04 +0200

    Added: TimeConverter Project

---
+++ TimeConverter/TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Roelf Sluman on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
