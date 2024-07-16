6f0c618447bc3005fa97248673f835cfa346ec84
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-16 08:24:20 +0200

    Project 2 - Guess The Flag: Intro - Custom Button

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,30 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    VStack(spacing: 20) {
-      Button("Button 1") { }
-        .buttonStyle(.bordered)
-      
-      Button("Button 2", role: .destructive) { }
-        .buttonStyle(.bordered)
-
-      Button("Button 3") { }
-        .buttonStyle(.borderedProminent)
-      
-      Button("Button 4", role: .destructive) { }
-        .buttonStyle(.borderedProminent)
-
-      Button("Button 5") { }
-        .buttonStyle(.bordered)
-        .tint(.indigo)
-      
-      Button("Button 6") { }
-        .buttonStyle(.borderedProminent)
-        .tint(.indigo)
-
-      Button("Delete selection") {
-        executeDelete()
-      }
+    Button {
+      print("Button was tapped")
+    } label: {
+      Text("Press here...")
+        .padding()
+        .foregroundStyle(.white)
+        .background(.red)
+        .cornerRadius(10)
     }
   }
   
