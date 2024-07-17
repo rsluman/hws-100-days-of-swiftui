34abe49807d01b7d9a7c13d133ca834a25f87619
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-17 10:15:53 +0200

    Project 3 - ViewsAndModifiers: Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Roelf Sluman on 17/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
