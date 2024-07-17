8673a25c57e526e241b64c69194132bb7a24fac0
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-17 14:45:13 +0200

    Project 3 - ViewsAndModifiers: View composition

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,41 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-
-  let t1 = Text("A simple constant")
-
-  // let wrongT1 = Text("Hello (not").padding().background(.red)
-  // ERROR: Property definition has inferred type 'some View', involving the 'some' return type of another declaration
-  
-  let t2: some View = Text("With one modifier").padding().background(.green)
-  
-  var t3: some View {
-    Text("Computed property")
-      .padding()
-      .background(.red)
-  }
-
-  @ViewBuilder var t4: some View {
-    Text("ViewBuilder")
-      .padding()
-      .background(.yellow)
-
-  }
-  
-  
   var body: some View {
-    VStack(spacing: 20) {
-      t1
-      t2
-      t3
-      t4
-      Button("Show self's type") {
-        print(type(of: self))
-      }
+    VStack(spacing: 10) {
+      CapsuleText(title: "First")
+      CapsuleText(title: "Second")
     }
   }
+ 
 }
 
 #Preview {
   ContentView()
+}
+
+struct CapsuleText: View {
+  var title: String
+  var body: some View {
+    Text(title)
+      .font(.largeTitle)
+      .padding()
+      .foregroundStyle(.white)
+      .background(.blue)
+      .clipShape(.capsule)
+  }
 }
