94301b48d530fbfd01f0cc74666730c3221cc845
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-17 10:42:37 +0200

    Project 3 - ViewsAndModifiers: What is behind the main SwiftUI view?

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack(alignment: .leading) {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
     }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(.red)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
