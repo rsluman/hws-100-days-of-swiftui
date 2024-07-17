2b064f46971ddfc18bb2fe37768d3cb8684f7186
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-17 15:06:19 +0200

    Project 3 - ViewsAndModifiers: Custom modifiers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,28 +7,59 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  var body: some View {
-    VStack(spacing: 10) {
-      CapsuleText(title: "First")
-      CapsuleText(title: "Second")
-    }
+extension View {
+  var titleStyle: some View {
+    self.modifier(Title())
   }
- 
+}
+
+struct Title: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .font(.largeTitle)
+      .foregroundStyle(.white)
+      .padding()
+      .background(.blue)
+      .clipShape(RoundedRectangle(cornerRadius: 15))
+  }
+}
+
+extension View {
+  func watermarked(with text: String) -> some View {
+    self.modifier(Watermark(text: text))
+  }
+  
+}
+
+
+struct Watermark: ViewModifier {
+  var text: String
+  
+  func body(content: Content) -> some View {
+    ZStack(alignment: .bottomTrailing) {
+      content
+      
+      Text(text)
+        .font(.caption)
+        .foregroundStyle(.white)
+        .padding(5)
+        .background(.black)
+    }
+    
+  }
+}
+
+
+struct ContentView: View {
+  
+  var body: some View {
+    Text("Hello world!")
+      .frame(width: 200, height: 200)
+      .background(.blue)
+      .watermarked(with: "Hacking with Swift")
+  }
 }
 
 #Preview {
   ContentView()
-}
-
-struct CapsuleText: View {
-  var title: String
-  var body: some View {
-    Text(title)
-      .font(.largeTitle)
-      .padding()
-      .foregroundStyle(.white)
-      .background(.blue)
-      .clipShape(.capsule)
-  }
 }
