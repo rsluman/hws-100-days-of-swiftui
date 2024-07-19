fb150b600f0d3c44f21dd3b0e58df4b61b6ff705
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-19 15:24:00 +0200

    Project 4 - BetterRest = Entering numbers with Stepper

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,17 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+  @State var sleepAmount = 8.0
+  
+  var body: some View {
+    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
