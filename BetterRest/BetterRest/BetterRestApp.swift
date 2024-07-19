7845890627f358a6530fe868d590e8e3ffa6e234
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-19 15:06:20 +0200

    Project 4 - BetterRest Initial Commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Roelf Sluman on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
