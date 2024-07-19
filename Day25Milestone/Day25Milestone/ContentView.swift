445a9ade70d420f40561bc435e5aae92bb180b15
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-19 11:18:13 +0200

    Milestone: Projects 1-3 - Key Points Custom Bindings

---
+++ Day25Milestone/Day25Milestone/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  Day25Milestone
+//
+//  Created by Roelf Sluman on 19/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  @State private var agreedToTerms = false
+  @State private var agreedToPrivacyPolicy = false
+  @State private var agreedToEmails = false
+  
+  var body: some View {
+    let agreedToAll = Binding<Bool>(
+      get: {
+        agreedToTerms && agreedToPrivacyPolicy && agreedToEmails
+      },
+      set: {
+        agreedToTerms = $0
+        agreedToPrivacyPolicy = $0
+        agreedToEmails = $0
+      }
+    )
+    
+    return VStack {
+      Toggle("Agree to terms", isOn: $agreedToTerms)
+      Toggle("Agree to privacy policy", isOn: $agreedToPrivacyPolicy)
+      Toggle("Agree to receive shipping emails", isOn: $agreedToEmails)
+      Toggle("Agree to all", isOn: agreedToAll)
+    }
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ Day25Milestone/Day25Milestone/Day25MilestoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day25MilestoneApp.swift
+//  Day25Milestone
+//
+//  Created by Roelf Sluman on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Day25MilestoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
