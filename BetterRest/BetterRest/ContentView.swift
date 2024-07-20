8ff76e219d4f472cba5ffb61451d6d672402f5cc
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-20 16:39:09 +0200

    Project 4 - BetterRest - Cleaning up the user interface

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -5,16 +5,29 @@
 //  Created by Roelf Sluman on 19/07/2024.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
-  @State private var wakeUp = Date.now
+  @State private var wakeUp = defaultWakeTime
   @State private var sleepAmount = 8.0
   @State private var coffeeAmount = 1
   
+  @State private var alertTitle = ""
+  @State private var alertMessage  = ""
+  @State private var isShowingAlert = false
+  
+  static private var defaultWakeTime: Date {
+    var components = DateComponents()
+    components.hour = 7
+    components.minute = 0
+    return Calendar.current.date(from: components) ?? .now
+  }
+  
+  
   var body: some View {
     NavigationStack {
-      List {
+      Form {
         Section("When do you want to wake up?") {
           DatePicker("Enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
             .labelsHidden()
@@ -26,15 +39,21 @@ struct ContentView: View {
             Text("\(sleepAmount.formatted()) hours")
           }
         }
-
+        
         Section("Daily Coffee Intake") {
           Stepper(value: $coffeeAmount, in: 0...20) {
-            Text("\(coffeeAmount.formatted()) cup\(coffeeAmount != 1 ? "s" : "")")
+            Text("^[\(coffeeAmount) cup](inflect: true)")
           }
         }
-
-
       }
+      .padding()
+      
+      .alert(alertTitle, isPresented: $isShowingAlert) {
+        Button("OK") { }
+      } message: {
+        Text(alertMessage)
+      }
+      
       .navigationTitle("BetterRest")
       .toolbar {
         Button("Calculate", action: calculateBedTime)
@@ -43,6 +62,29 @@ struct ContentView: View {
   }
   
   func calculateBedTime() {
+    let c = Calendar.current
+    
+    do {
+      let config = MLModelConfiguration()
+      let model = try SleepCalculator(configuration: config)
+      
+      let components = c.dateComponents([.hour, .minute], from: wakeUp)
+      let hour = components.hour ?? 0 * 60 * 60
+      let minute = components.minute ?? 0 * 60
+      
+      let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+      
+      let sleepTime = wakeUp - prediction.actualSleep
+      
+      alertTitle = "Your ideal bedtime is …"
+      alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+    } catch {
+      // something went wrong
+      alertTitle = "Error"
+      alertMessage = "Sorry, there was a problem calculating your bedtime."
+    }
+    
+    isShowingAlert = true
     
   }
 }
