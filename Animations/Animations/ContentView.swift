f165f9c15a88d7253a0951261dcc9a1456bb4dca
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-21 14:43:52 +0200

    Project 6 - Animation - Creating implicit animations (B)

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -12,21 +12,22 @@ struct ContentView: View {
   
   var body: some View {
     Button("Tap Me!") {
-//      withAnimation(.spring(duration: 1, bounce: 0.9)) {
-        animationAmount += 1
-//      }
+//      animationAmount += 1
     }
     .padding(50)
     .background(.red)
     .foregroundStyle(.white)
     .clipShape(Circle())
-    .scaleEffect(animationAmount)
-    .blur(radius: (animationAmount - 1) * 3)
-//    .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
-//    .animation(.easeInOut(duration: 2), value: animationAmount)
-//    .animation(.easeInOut(duration: 2).delay(1), value: animationAmount)
-//    .animation(.easeInOut(duration: 2).repeatCount(3, autoreverses: true), value: animationAmount)
-    .animation(.easeInOut(duration: 2).repeatForever(autoreverses: true), value: animationAmount)
+    .overlay(
+      Circle()
+        .stroke(.red)
+        .scaleEffect(animationAmount)
+        .opacity(2 - animationAmount)
+        .animation(.easeOut(duration: 1).repeatForever(autoreverses: false), value: animationAmount)
+    )
+    .onAppear {
+      animationAmount = 2
+    }
   }
 }
 
