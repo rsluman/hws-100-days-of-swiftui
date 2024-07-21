3bcee42b8472794813e930b17444bd92fc2238b5
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-21 14:00:41 +0200

    Project 6 - Animation - Initial Commit

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Roelf Sluman on 21/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
