4818756da50bfccfad5e814ecb3f77bd953490ae
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-21 10:37:47 +0200

    Project 5 - Word Scramble - Working with strings

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,17 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
     }
+    .padding()
+    .onAppear {
+      testStrings()
+    }
+  }
+  
+  func testStrings() {
+//    let input = """
+//                a
+//                b
+//                c
+//                """
+//    
+//    let trimmedString = input.trimmingCharacters(in: .whitespaces)
+//    let stringArray = trimmedString.components(separatedBy: "\n")
+//    let randomString = stringArray.randomElement()
+//    print(randomString!)
+    
+    let word = "swift"
+    let checker = UITextChecker()
+    let range = NSRange(location: 0, length: word.utf16.count)
+    let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+    
+    let allGood = misspelledRange.location == NSIntegerMax
+    print(allGood)
+    print(misspelledRange.location)
+    print(NSNotFound == misspelledRange.location)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
