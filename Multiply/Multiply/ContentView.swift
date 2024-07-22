d9d4cdb2b2882408b19af6bc29526848133f4ff5
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-22 16:17:21 +0200

    Milestone: Projects 4-6 - Challenge Part 2: Game logic is now working.

--- Multiply/Multiply/ContentView.swift
+++ Multiply/Multiply/ContentView.swift
@@ -23,13 +23,14 @@ struct ContentView: View {
   ]
   
   @State private var gameState = GameState.setup
-  @State private var highestTable = 2 // highest table to practice
+  @State private var highestTableSelected = 2 // highest table to practice
   @State private var numberOfQuestions = 5 // number of questions for this game
   
-  @State private var currentQuestion = 0 // current question in this game
+  @State private var currentQuestion = 0 // current question
   @State private var multiplicand = 0
   @State private var multiplier = 0
-  @State private var answer: Int?
+  @State private var answer = "" // current answer
+  @State private var submittedAnswers = [Bool]() // true: answer was correct
   @State private var score = 0
   
   var body: some View {
@@ -40,69 +41,124 @@ struct ContentView: View {
         
         switch gameState {
         case .setup:
-          VStack(alignment: .leading) {
-            Text("How difficult?")
-              .font(.title)
-            Stepper("Highest Table: \(highestTable)", value: $highestTable, in: 2...maxTableLimit)
-              .padding(.bottom, 40)
-            
-            Text("How many Questions?")
-              .font(.title)
-            Picker("How Many Questions?", selection: $numberOfQuestions) {
-              ForEach(possibleQuestionCount, id: \.self) {
-                Text("\($0)")
-              }
-            }
-            .pickerStyle(.segmented)
-            .padding(.bottom, 40)
-            
-            HStack {
-              Spacer()
-              
-              Button {
-                gameState = .playing
-              } label: {
-                Label("Play!", systemImage: "play.circle.fill")
-                  .padding(30)
-                  .background(.blue)
-                  .foregroundColor(.white)
-                  .clipShape(.capsule)
-                
-              }
-              Spacer()
-            }
-            Spacer()
-          }
-          .padding()
-          
+          setupView
           
         case .playing:
-          VStack {
-            
-          }
-          
-          
+          playingView
           
         case .gameOver:
-          Text("\(gameState)")
+          gameOverView
         }
       }
       .navigationTitle("Multiply")
       .toolbar {
-        Button("New Game") { gameState = .setup }
+        Button("New Game", action: newGame)
       }
     }
   }
   
+  @ViewBuilder var setupView: some View {
+    VStack(alignment: .leading) {
+      Text("How good are you?")
+        .font(.title)
+      Stepper("Game Levels: \(highestTableSelected)", value: $highestTableSelected, in: 2...maxTableLimit)
+        .padding(.bottom, 40)
+      
+      Text("How many Questions?")
+        .font(.title)
+      Picker("How Many Questions?", selection: $numberOfQuestions) {
+        ForEach(possibleQuestionCount, id: \.self) {
+          Text("\($0)")
+        }
+      }
+      .pickerStyle(.segmented)
+      .padding(.bottom, 40)
+      
+      HStack {
+        Spacer()
+        
+        StartButton(action: startGame)
+        
+        Spacer()
+      }
+      Spacer()
+    }
+    .padding()
+  }
+  
+  @ViewBuilder var playingView: some View {
+    VStack {
+      progressBar
+      
+      
+      HStack {
+        Spacer()
+        Text("\(multiplicand) x \(multiplier) =")
+          .font(.largeTitle)
+        TextField("Your answer?", text: $answer)
+          .font(.largeTitle)
+          .onSubmit(processAnswer)
+        
+        
+        Spacer()
+      }
+      .padding()
+      
+      Spacer()
+      Text("Your Score: \(score)")
+        .font(.title)
+    }
+  }
+  
+  @ViewBuilder var gameOverView: some View {
+    VStack {
+      progressBar
+      Spacer()
+      Text("Your Score: \(score)")
+        .font(.largeTitle)
+      Spacer()
+      Text("How about another one?")
+        .font(.title3)
+      StartButton(action: newGame)
+    }
+  }
+  
+  @ViewBuilder var progressBar: some View {
+    HStack {
+      ForEach(1...numberOfQuestions, id: \.self) { question in
+        Rectangle()
+          .fill(progressBarItemColorForQuestion(question))
+      }
+    }
+    .frame(height: 20)
+    .padding()
+  }
+  
+  func progressBarItemColorForQuestion(_ question: Int) -> Color {
+    if question == currentQuestion {
+      return .yellow
+    } else if question > currentQuestion {
+      return .gray
+    } else {
+      // answer has already been given
+      return submittedAnswers[question - 1] ? .green : .red
+    }
+  }
+  
+  func newGame() {
+    gameState = .setup
+  }
+  
   func startGame() {
+    gameState = .playing
     score = 0
     currentQuestion = 0
-    
+    nextRound()
   }
   
   func nextRound() {
-    if currentQuestion < numberOfQuestions {
-      currentQuestion += 1
+    currentQuestion += 1
+    if currentQuestion <= numberOfQuestions {
       makeQuestion()
     } else {
       gameState = .gameOver
@@ -110,15 +166,23 @@ struct ContentView: View {
   }
   
   func makeQuestion() {
+    answer = ""
     multiplicand = Int.random(in: 1...10)
-    multiplier = Int.random(in: 2...highestTable)
+    multiplier = Int.random(in: 2...highestTableSelected)
   }
   
-  func processAnswer(_ answer: Int?) {
-    guard let answer else { return } // no answer given
+  func processAnswer() {
+    guard !answer.isEmpty else { return } // no answer given
+    
+    guard let answer = Int(answer) else { return } // no int value
     
     if isCorrectAnswer(answer) {
+      submittedAnswers.append(true)
       increaseScore()
+      nextRound()
+    } else {
+      // still to come: wrong answer!
+      submittedAnswers.append(false)
       nextRound()
     }
   }
@@ -128,12 +192,26 @@ struct ContentView: View {
   }
   
   func increaseScore() {
-    score += maxTableLimit - 1
+    score += (highestTableSelected - 1)
   }
   
+}
+
+struct StartButton: View {
+  var action: () -> (Void)
   
+  var body: some View {
+    Button(action: action) {
+      Label("Play!", systemImage: "play.circle.fill")
+        .padding(30)
+        .background(.blue)
+        .foregroundColor(.white)
+        .clipShape(.capsule)
+    }
+  }
 }
 
 #Preview {
   ContentView()
 }
+
