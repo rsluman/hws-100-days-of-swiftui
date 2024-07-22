9401d3ee6b61a7aa96c2b22b1d9b34ed955f3960
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-22 17:36:23 +0200

    Project 7 - iExpense - Introduction initial Commit

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Roelf Sluman on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
