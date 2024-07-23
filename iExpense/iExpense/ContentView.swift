25e97564fcc10f552ea7418e70ec36f4da4c7dca
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-23 10:55:09 +0200

    Project 7 - iExpense - Building a list we can delete from My version, with entry View

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,20 +7,60 @@
 
 import SwiftUI
 
-struct User: Codable {
-  let firstName: String
-  let lastName: String
+struct ExpenseItem {
+  let name: String
+  let type: String
+  let amount: Double
+}
+
+@Observable
+class Expenses {
+  var items = [ExpenseItem]()
 }
 
 struct ContentView: View {
-  @State private var user = User(firstName: "Taylor", lastName: "Swift")
+  @State var expenses = Expenses()
   
   var body: some View {
-    Button("Save User") {
-      let encoder = JSONEncoder()
-      
-      if let data = try? encoder.encode(user) {
-        UserDefaults.standard.setValue(data, forKey: "UserData")
+    NavigationStack {
+        List {
+          ForEach(expenses.items, id: \.name) { item in
+            Text(item.name)
+          }
+          .onDelete() { expenses.items.remove(atOffsets: $0) }
+      }
+      .navigationTitle("iExpense")
+      .toolbar {
+        NavigationLink("Add Item", destination: AddItemView(expenses: expenses))
+      }
+    }
+  }
+}
+
+struct AddItemView: View {
+  @Environment(\.dismiss) private var dismiss
+  
+  @State var expenses: Expenses
+  
+  @State var itemName = ""
+  @State var itemType = ""
+  @State var itemAmount = ""
+
+  var body: some View {
+    Form {
+      TextField("Name", text: $itemName)
+      TextField("Type", text: $itemType)
+      TextField("Amount", text: $itemAmount)
+        .keyboardType(.decimalPad)
+    }
+    .toolbar {
+      Button("Save") {
+        if let itemAmount = Double(itemAmount), !itemName.isEmpty && !itemType.isEmpty && !itemAmount.isZero  {
+          expenses.items.append(ExpenseItem(name: itemName, type: itemType, amount: itemAmount))
+        }
+        dismiss()
+        return
+        
       }
     }
   }
