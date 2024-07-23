e80c2c56ce1dd5ffe26c7672cee36b8130c333b4
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-23 14:01:33 +0200

    Project 8 - Moonshot - Introduction Initial Commit

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Roelf Sluman on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
