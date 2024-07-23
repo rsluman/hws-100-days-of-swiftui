c2b668f060d14105d38a48195a8926e2011eed1e
Roelf Sluman <roelf@cryptocoiners.nl>
2024-07-23 10:06:10 +0200

    Project 7 - iExpense - Storing user settings with UserDefaults Using @AppStorage property wrapper instead of UserDefaults.standard

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,13 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+  @AppStorage("tapCount") private var tapCount = 0
   
   var body: some View {
     Button("Tap Count: \(tapCount)") {
       tapCount += 1
-      UserDefaults.standard.setValue(tapCount, forKey: "Tap")
-      
     }
   }
 }
